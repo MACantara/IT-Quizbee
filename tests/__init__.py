d2bcf90c346147e50a43030e297992e6592be29d
MACantara <cantara.michaelangelo@gmail.com>
2025-10-17 10:22:35 +0800

    Add initial test suite documentation for IT Quizbee using Playwright

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+"""
+IT Quizbee Test Suite
+
+This package contains automated end-to-end tests for the IT Quizbee web application.
+Tests are organized by feature/page and use Playwright for browser automation.
+"""
