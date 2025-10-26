b315fa99c98ba6254dbdbd2bc1962620fbc1a30e
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2025-10-26 23:00:37 +0800

    Refactor utility functions to include error handlers in the module's exports

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -2,4 +2,26 @@
 Utility Functions Package
 """
 
-__all__ = []
+from .error_handlers import (
+    APIError,
+    ValidationError,
+    NotFoundError,
+    AuthenticationError,
+    AuthorizationError,
+    ServerError,
+    handle_error,
+    error_response,
+    success_response
+)
+
+__all__ = [
+    'APIError',
+    'ValidationError',
+    'NotFoundError',
+    'AuthenticationError',
+    'AuthorizationError',
+    'ServerError',
+    'handle_error',
+    'error_response',
+    'success_response'
+]
